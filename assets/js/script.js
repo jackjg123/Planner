25866686b8b6ab48fcebc4aada87a45cc42a54d1
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-26 00:34:22 -0400

    Started over. I added the current day function to dispay the current day at the top of the page. I also added the time-block function in order to add the appropiate color to all individual  time blocks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-// This displays the current day of the month and the current year.
-  var today = dayjs();
-$('#1a').text(today.format('MMM D, YYYY'));
-});
+// Save button. When the Button with the Save icon is clicked the user's text is saved to local storage.
 
-// Starter code. 
-
+// Displays today's date.
 $(document).ready(function () {
-  $('.saveBtn').on('click', function () {
-    // get nearby values
-  });
-  function hourUpdater() {
-    var currentHour = dayjs().hour();
-    // loop over time blocks
-    $('.time-block').each(function () {
-    });
-  }
-  hourUpdater();
-  setInterval(hourUpdater, 15000);
-  // load any saved data from localStorage
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  // display current day on page
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+  var currentHour = dayjs().hour();
+
+  // compares current time to each block.
+  $('.time-block').each(function () {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+    // Add color to each time-block
+    if (blockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (blockHour === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
 });
