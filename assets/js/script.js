13f61e08c24ed268e06f0fdb852ac80a4990da3f
Jack <131559096+jackjg123@users.noreply.github.com>
2023-06-27 16:42:45 -0400

    fixed time block ids in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // Save button. When the Button with the Save icon is clicked the user's text is saved to local storage.
 
-// Displays today's date.
+// Displays today's date. After DOM has loaded.
 $(document).ready(function () {
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
   var currentHour = dayjs().hour();
-
-  // compares current time to each block.
+  console.log(currentHour);
+  // Compare current-time to each block.
   $('.time-block').each(function () {
-    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+    var blockHour = parseInt($(this).attr('id'));
 
     // Add color to each time-block
     if (blockHour < currentHour) {
@@ -16,6 +16,25 @@ $(document).ready(function () {
       $(this).addClass('present');
     } else {
       $(this).addClass('future');
+    }
+    console.log($('.time-block'));
+  });
+
+  // load
+  $('.saveBtn').on('click', function () {
+    var hour = $(this).parent().attr('id');
+    var description = $(this).siblings('.description').val();
+    console.log(hour + description);
+
+    localStorage.setItem(hour, description);
+  });
+
+  $('.time-block').each(function () {
+    var hour = $(this).attr('id');
+    var description = localStorage.getItem(hour);
+
+    if (description !== null) {
+      $(this).find('.description').val(description);
     }
   });
 });
